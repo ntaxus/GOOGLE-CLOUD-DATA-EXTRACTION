fcdb87734240d9fc7fd5367851c4117baa36773d
Esteban <encinaesteban27@gmail.com>
2021-10-07 20:08:24 -0300

    Process.py

---
+++ process.py
@@ -0,0 +1,21 @@
+#Importing libraries
+import pandas as pd
+from google.cloud import bigquery
+import json
+import os 
+
+def run():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+if '__name__'=='__main__':
+    run()
